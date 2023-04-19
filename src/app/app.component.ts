c70fcefdcfc51630f1fd70e4591c8793a47a42cc
AdamHamdi <prodev.webdesign@gmail.com>
2023-04-19 14:33:07 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Guid } from 'guid-typescript';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
+import { Todo } from 'src/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list-app';
+  todos: Todo[] = []
+  task: string = '';
+  taskCompleted: Todo[] = []
+  isEmptyInput(): boolean {
+    return this.task.trim() === ''
+  }
+  nbTaskCompleted: number = 0
+  addTask() {
+    let TodoFirst = new Todo(
+      Guid.create(),
+      this.task,
+      false)
+
+    this.todos.push(TodoFirst);
+    this.task = ''
+  }
+  markAsCompleted(id: Guid) {
+    // let todo = this.todos.filter(todo => todo.id === id)[0];
+  let todo =this.todos.find(todo =>todo.id===id);
+  if(todo)
+    todo.isComplete = true;
+    this.nbTaskCompleted++
+  }
+  markAsNotCompleted(id: Guid) {
+    // let todo = this.todos.filter(todo => todo.id === id)[0];
+    let todo =this.todos.find(todo =>todo.id===id);
+  if(todo)
+    todo.isComplete = false;
+    this.nbTaskCompleted--
+  }
+  deleteTask(id: Guid) {
+    let index = this.todos.findIndex(todo => todo.id === id);
+    if (index !== -1) {
+      this.todos.splice(index, 1)
+    }
+
+  }
+ 
+
 }
+let array1: number[]=[1,2,3,4,5,6,7,8,9];
+let array2: string[]=['E','A','Z','F','T','S','Q','C','X'];
+for (let index = 0; index < array1.length; index++){
+  const element=array1[index]
+ console.log('element', element)
+}
+array2.forEach(index =>{
+  console.log("index",index)
+})
+let x;
+x=25
+x=5
+console.log("x is", x);
+
+class Student{
+  name!: string;
+  constructor(studentName: string){
+    this.name = studentName
+  }
+  welcome(){
+    console.log(`Welcome ${this.name}`);
+    
+  }
+}
+const s1 = new Student('Adam')
+console.log(s1.name);
+
+s1.welcome()
